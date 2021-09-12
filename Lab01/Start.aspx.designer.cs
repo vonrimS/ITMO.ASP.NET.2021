60f5a7f54cd8374e5e8e9c79c26eb81a0bd93ead
vonrimS <mr.mytab@gmail.com>
2021-09-12 15:31:55 +0300

    Lab 06

--- Lab01/Start.aspx.designer.cs
+++ Lab01/Start.aspx.designer.cs
@@ -13,5 +13,23 @@ namespace Lab01
 
     public partial class Start
     {
+
+        /// <summary>
+        /// ScriptManager1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
+
+        /// <summary>
+        /// UpdatePanel1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
     }
 }
